5351d2bb6f2568646e52d28314aec3578c89f3bc
VaibhavShejul123 <shejulv12@gmail.com>
2023-04-21 21:38:58 +0530

    this is third commit

---
+++ src/dicisionmakingstatement/IfDemo.java
@@ -0,0 +1,23 @@
+package dicisionmakingstatement;
+
+import java.util.Scanner;
+
+public class IfDemo {
+ 
+public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any number");
+		int number=sc.nextInt();
+		
+		if(number >=10) {
+			System.out.println("Number is greater than 10");
+			
+		}
+		
+		
+		
+	
+	}
+}
+
